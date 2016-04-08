fbe3edf8f597cbd1a3eed75f7f7ae1e350ca18e2
cnsnyder <csnydini@gmail.com>
2016-04-07 22:36:22 -0400

    WIP

---
+++ mailchimp.ts
@@ -0,0 +1,79 @@
+import http = require('http');
+export function listSubscribe(email: string, key: string, listId: string, callback: void):void {
+    var status = "";
+    var headers = "";
+    var response_data = "";
+    var post_data = JSON.stringify({email_address:email,
+                                    status:"pending"});
+    console.log(post_data);
+    var post_options = {
+        host: 'us1.api.mailchimp.com',
+        port: '80',
+        path: '/3.0/lists/' + listId + '/members/',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': Buffer.byteLength(post_data),
+            'Authorization': 'apikey ' + key
+        }
+    };
+    var req = http.request(post_options, function(res) {
+      status = res.statusCode;
+      headers = JSON.stringify(res.headers)
+      console.log('STATUS: ' + status);
+      console.log('HEADERS: ' + headers);
+      res.setEncoding('utf8');
+      res.on('data', function (chunk) {
+        response_data += chunk;
+        console.log('BODY: ' + chunk);
+      });
+      res.on('end', function() {
+        console.log('No more data in response.');
+        callback(response_data, headers, status);
+      });
+    });
+    req.on('error', function(e) {
+      console.log('problem with request: ' + e);
+    });
+    req.write(post_data);
+    req.end();
+  };
+exports.listSubscribe = function(email, key, listId, callback) {
+  var status = "";
+  var headers = "";
+  var response_data = "";
+  var post_data = JSON.stringify({email_address:email,
+                                  status:"pending"});
+  console.log(post_data);
+  var post_options = {
+      host: 'us1.api.mailchimp.com',
+      port: '80',
+      path: '/3.0/lists/' + listId + '/members/',
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+          'Content-Length': Buffer.byteLength(post_data),
+          'Authorization': 'apikey ' + key
+      }
+  };
+  var req = http.request(post_options, function(res) {
+    status = res.statusCode;
+    headers = JSON.stringify(res.headers)
+    console.log('STATUS: ' + status);
+    console.log('HEADERS: ' + headers);
+    res.setEncoding('utf8');
+    res.on('data', function (chunk) {
+      response_data += chunk;
+      console.log('BODY: ' + chunk);
+    });
+    res.on('end', function() {
+      console.log('No more data in response.');
+      callback(response_data, headers, status);
+    });
+  });
+  req.on('error', function(e) {
+    console.log('problem with request: ' + e);
+  });
+  req.write(post_data);
+  req.end();
+};
---
+++ server.ts
@@ -0,0 +1,76 @@
+/// <reference path="./typings/express/express.d.ts"/>
+/// <reference path="./typings/body-parser/body-parser.d.ts"/>
+/// <reference path="./typings/serve-favicon/serve-favicon.d.ts"/>
+import express = require('express');
+import bodyParser = require('body-parser');
+import fs = require("fs");
+// TODO: Convert to ts
+import * as mailchimp from './mailchimp.ts';
+import * as test from './test.ts';
+import path = require('path');
+import favicon = require('serve-favicon');
+const debug = process.env.MTAPP_DEBUG;
+
+// Try to load the keys
+var keys = undefined;
+try {
+  keys = JSON.parse(fs.readFileSync('keys.json', 'utf8'));
+}
+catch(err) {
+  console.log(err);
+}
+if (keys === undefined && debug !== undefined) {
+  try {
+    keys = JSON.parse(fs.readFileSync('.keys.json', 'utf8'))
+  }
+  catch(err) {
+    console.log(err);
+  }
+}
+
+// Set up express
+var app = express();
+
+app.use(favicon(path.join(__dirname, 'public', 'assets', 'favicon.png')));
+app.set('views', __dirname + "/views");
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+
+// Routes
+app.use('/', express.static('public/index.html'));
+app.use('/privacy_policy', express.static('public/static/privacy_policy.html'));
+
+// Forward the submission to mailchimp after adding our api key
+app.post('/subscribe', function (req, res) {
+  console.log("Processing request with body: %s", JSON.stringify(req.body));
+  // send to mailchimp
+  console.log(keys);
+  mailchimp.listSubscribe(req.body.email_address,
+                          keys.mailchimp.key,
+                          keys.mailchimp.listId,
+                          function (response_data, headers, status){
+                            console.log('SERVER response_data: %s', response_data);
+                            if (status == '200'){
+                              res.json({success: true, displayMessage: "Check email to confirm."});
+                            } else if (status == '400'){
+                              res.json({"success": false, displayMessage: "This email is already subscribed."})
+                            }
+                          });
+});
+app.use(function(req, res, next) {
+  res.status(404);
+  if (req.accepts('html')) {
+      res.render('404', {url: req.url });
+  }
+});
+
+var server = app.listen(80, function() {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('App listening at http://%s:%s', host, port);
+});

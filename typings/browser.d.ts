e5d1d3c72bdb0b9951e9b22012afc30b71106ae5
cnsnyder <csnydini@gmail.com>
2016-04-09 15:56:32 -0400

    Switch to typings from tsd

---
+++ typings/browser.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="browser/ambient/body-parser/index.d.ts" />
+/// <reference path="browser/ambient/express/index.d.ts" />
+/// <reference path="browser/ambient/node/index.d.ts" />
+/// <reference path="browser/ambient/serve-favicon/index.d.ts" />
